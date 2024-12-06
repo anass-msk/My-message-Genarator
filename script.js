954efc485fac85c01877cfab0c7b136c8cb99854
anass meskini <anass.msk@gmail.com>
2024-12-06 13:12:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const today = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday' ]
+
+const yourLuck =['Dumb luck', 'Luck from Motion' , 'Luck from Awareness' ,'Luck from Unique Character', 'so lucky']
+
+const youCanBe =['Be grateful for the things you have', 'Make someone smile','Eat at least one healthy meal', 'Spend at least a few minutes outdoors', 'Say positive things to yourself in the mirror']
+
+
+console.log(youCanBe[1])
