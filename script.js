18d13ed1b95c9dac79456e7dc3695acc3882730f
anass meskini <anass.msk@gmail.com>
2024-12-06 13:34:30 +0100

    second commit

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const today = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday' ]
+function generateRandomNum(num){
+    return Math.floor(Math.random() *num)
+}
 
-const yourLuck =['Dumb luck', 'Luck from Motion' , 'Luck from Awareness' ,'Luck from Unique Character', 'so lucky']
+const todayLack = {
+    today : ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday' ],
+    yourLuck : ['Dumb luck', 'Luck from Motion' , 'Luck from Awareness' ,'Luck from Unique Character', 'so lucky'],
+    youCan : ['Be grateful for the things you have', 'Make someone smile','Eat at least one healthy meal', 'Spend at least a few minutes outdoors', 'Say positive things to yourself in the mirror']
 
-const youCanBe =['Be grateful for the things you have', 'Make someone smile','Eat at least one healthy meal', 'Spend at least a few minutes outdoors', 'Say positive things to yourself in the mirror']
+}
 
+
+let personalLuck = []
 
 console.log(youCanBe[1])
